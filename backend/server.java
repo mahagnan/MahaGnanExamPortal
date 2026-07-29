bb4f06174e928c702b5196d55dd1f7d061d941cd
mahagnan <mahagnan01@gmail.com>
2026-07-29 14:44:17 +0530

    Update server.java

--- backend/server.java
+++ backend/server.java
@@ -1,1 +1,13 @@
-x
+package com.mahagnan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MahagnanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MahagnanApplication.class, args);
+    }
+
+}
